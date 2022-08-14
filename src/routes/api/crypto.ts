fb82e391e3ab3c888165857085679fe28f9c59c2
Beck-R <Beckham.Reimer@gmail.com>
2022-08-14 14:41:18 -0500

    fixed .env reading

--- src/routes/api/crypto.ts
+++ src/routes/api/crypto.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: "../../.env"});
+dotenv.config({ path: ".env"});
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export const GET = () => {
